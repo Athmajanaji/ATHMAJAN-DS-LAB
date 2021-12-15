98c3b1fac90a8e40caf9d24dacb806b2a9eae7dc
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2021-12-15 09:27:06 +0530

    linkedlist

---
+++ linkedlist.c
@@ -0,0 +1,193 @@
+#include<stdlib.h>
+#include <stdio.h>
+     
+void create();
+void display();
+struct node *insertatend(struct node *head, int data);
+struct node *insertatbeg(struct node*head,int data);
+struct node *insertatindex(struct node*head,int data,int pos);
+struct node *deleteatbeg(struct node *head);
+struct node *deleteatend(struct node *head);
+void deleteatpos(struct node *head,int pos);
+ 
+struct node
+{
+ int data;
+ struct node *next;
+};
+struct node *start=NULL;
+int main()     
+{
+ int choice,a,pos;
+ while(1){
+  printf("\n                MENU                             \n");
+  printf("\n 1.Create     \n");
+  printf("\n 2.Insert At The Begining     \n");
+  printf("\n 3.Insert At End     \n");
+  printf("\n 4.Insert in between     \n");
+  printf("\n 5.Display    \n");
+  printf("\n 6.Delete at begining    \n");
+  printf("\n 7.Delete at end    \n");
+  printf("\n 8.Delete at Pos       \n");
+  printf("\n 9.Exit       \n");
+  printf("Enter your choice:\t");
+  scanf("%d",&choice);
+  switch(choice)
+  {
+  case 1:
+         create();
+         break;
+  case 5:
+         display();
+         break;                        
+  case 2:
+         printf("Enter Element ");
+         scanf("%d",&a);
+         start=insertatbeg(start,a);
+         break;
+  case 4:
+         printf("Enter Element ");
+         scanf("%d",&a);
+         printf("Enter Postion After Which you want to insert element");
+         scanf("%d",&pos);
+         start=insertatindex(start,a,pos);
+         break;
+  case 3:
+         printf("Enter Element ");
+         scanf("%d",&a);
+         start=insertatend(start,a);
+         break;
+  case 6:
+         start=deleteatbeg(start);
+         break;
+  case 7:
+         start=deleteatend(start);
+         break;
+  case 8:printf("Enter The POsition");
+         scanf("%d",&pos);
+         deleteatpos(start,pos);
+         break;
+  case 9:
+         exit(0);
+         break;
+                             
+  default:
+          printf("n Wrong Choice:n");
+          break;
+  }
+ }
+
+return 0;
+}
+
+void create()
+{
+ struct node *temp,*ptr;
+ temp=(struct node *)malloc(sizeof(struct node));
+ if(temp==NULL)
+ {
+  printf("\nOut of Memory Space:\n");
+  exit(0);
+ }
+ printf("\nEnter the data value for the node:\t");
+ scanf("%d",&temp->data);
+ temp->next=NULL;
+ if(start==NULL)
+ {
+  start=temp;
+ }
+ else
+ {
+  ptr=start;
+  while(ptr->next!=NULL)
+  {
+   ptr=ptr->next;
+  }
+   ptr->next=temp;
+  }
+}
+void display()
+{
+ struct node *ptr;
+ if(start==NULL)
+ {
+ printf("\nList is empty:\n");
+ return;
+ }
+ else
+ {
+  ptr=start;
+  printf("\nThe List elements are:\n");
+  while(ptr!=NULL)
+  {
+   printf("%d---->",ptr->data );
+   ptr=ptr->next ;
+  }
+ }
+}
+
+
+struct node *insertatend(struct node *head, int data)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    struct node *p = head;
+    ptr->data = data;
+    ptr->next = NULL;
+    while(p->next!=NULL){
+     p=p->next;
+    }
+    p->next=ptr;
+    return head;
+}
+
+struct node *insertatbeg(struct node*head,int data){
+    struct node *ptr=(struct node*)malloc(sizeof(struct node));
+    ptr->data = data;
+    ptr->next = start;
+    return ptr;
+}
+
+struct node *insertatindex(struct node*head,int data,int pos){
+    int i=0;
+    struct node *ptr=(struct node*)malloc(sizeof(struct node));
+    struct node *p=head;
+    ptr->data = data;
+    while(i!=pos-1){
+    p=p->next;
+    i++;
+    }
+    ptr->next = p->next;
+    p->next=ptr;
+    return head;
+}
+
+struct node *deleteatbeg(struct node *head){
+struct node *temp= head;
+struct node *p;
+head=head->next;
+free(temp);
+return head;
+}
+struct node *deleteatend(struct node *head){
+struct node *temp= head;
+struct node *p;
+while(temp->next!=NULL){
+p=temp;
+temp=temp->next;
+}
+p->next=NULL;
+free(temp);
+return head;
+}
+void deleteatpos(struct node *head,int pos){
+	int val;
+	struct node *temp=head,*p;
+	for(int i=1;i<pos;i++){
+		p=temp;
+		temp=temp->next;
+	}
+	val=temp->data;
+	p->next=temp->next;
+	free(temp);
+
+}
